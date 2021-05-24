9b9a94ac00c074a6f44584cf2eab29a78f1cf25c
MarceloCFSF <marcelo.cfsf@gmail.com>
2021-05-24 16:29:45 -0300

    Add 12.Invert_a_Binary_Tree

---
+++ 12.Invert_a_Binary_Tree/solution.py
@@ -0,0 +1,34 @@
+class Node:
+  def __init__(self, value):
+    self.left = None
+    self.right = None
+    self.value = value
+  def preorder(self):
+    print(self.value, end=' '),
+    if self.left: self.left.preorder()
+    if self.right: self.right.preorder()
+
+def invert(node):
+  if (node):
+    left = node.left
+    node.left = node.right
+    node.right = left
+
+    invert(node.right)
+    invert(node.left)
+  # Fill this in.
+
+root = Node('a') 
+root.left = Node('b') 
+root.right = Node('c') 
+root.left.left = Node('d') 
+root.left.right = Node('e') 
+root.right.left = Node('f') 
+
+root.preorder()
+# a b d e c f 
+print("\n")
+invert(root)
+root.preorder()
+print("\n")
+# a c f b e d
